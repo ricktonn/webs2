2233090dea49b4b270936fe699a1d01ccc936025
Unknown <ricktonneijk@gmail.com>
2018-03-31 21:21:36 +0200

    small

--- shop/resources/views/home.blade.php
+++ shop/resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+<h2>News</h2>
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -27,7 +27,7 @@
                 </div>
         </div>
                 <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" style="color:blue" aria-hidden="true"></span>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
--- shop/resources/views/pay.blade.php
+++ shop/resources/views/pay.blade.php
@@ -5,6 +5,7 @@
         <div class="container">
             <h1>Payment</h1>
             <div class="row">
+                <div class="container">
             <select>
                 <option>Payment method1</option>
                 <option>Payment method2</option>
@@ -14,12 +15,17 @@
                 <option>Payment method6</option>
                 <option>Payment method7</option>
             </select>
+                </div>
             </div>
             <div class="row">
+                <div class="container">
             <strong>Total price: ${{$total}}</strong>
+                </div>
+                <div class="container">
             <a href="{{ route("goSucces") }}">
                 <button type="button" class="btn btn-success"> Pay </button>
             </a>
+                </div>
             </div>
         </div>
 @endsection()
